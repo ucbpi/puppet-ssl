66175bc5ff5148b05e416df8951fcb625b6a560e
Aaron Russo <aaron.n.russo@gmail.com>
2014-10-02 02:59:20 -0700

    (rspec) cleanup ssl::incommon tests

--- spec/defines/ssl_incommon_spec.rb
+++ spec/defines/ssl_incommon_spec.rb
@@ -1,18 +1,13 @@
 require 'spec_helper'
 
 describe 'ssl::incommon', :type => :define do
-  # while we use RedHat osfamily, the entirety of this define should be
-  # OS independant. We just need to include the osfamily fact to appease
-  # the params class which will fail if it's not a supported osfamily
-  let :facts do
-    {
-      :osfamily => 'RedHat',
-    }
-  end
   let :title do
     'www.example.com'
   end
-  context 'no id passed' do
+
+  it { should contain_ssl__incommon('www.example.com') }
+
+  context 'with no id passed' do
     it do
       should contain_file('/usr/local/sbin/check-incommon-cert').with({
         'ensure' => 'present',
@@ -27,16 +22,16 @@ describe 'ssl::incommon', :type => :define do
     end
   end
 
-  context 'id 12345 passed' do
-    let :params do
-      {
-        :id => '12345',
-      }
-    end
+  context 'with an id passed' do
+    let(:params) {{ :id => '12345' }}
+
     it do
-      should contain_exec('get-cert-www.example.com').with({
-        'require' => 'File[/usr/local/sbin/check-incommon-cert]',
-      })
+      should \
+        contain_exec('get-cert-www.example.com').
+        with({
+          'require' => 'File[/usr/local/sbin/check-incommon-cert]',
+        })
+
       should contain_exec('get-int-www.example.com')
     end
   end
